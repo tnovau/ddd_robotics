51de20ac198dd41ecb238a35704bf2e7512069b2
Tomás Novau Foresi <tnovau@gmail.com>
2023-11-29 13:47:00 +0100

    #1 - Adding cors to API

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import { graphqlHTTP } from "express-graphql"
 import { buildSchema } from "graphql"
+import cors from "cors";
 import {
     createClient
 } from "../common/infrastructure/db.js"
@@ -99,6 +100,8 @@ const root = {
 }
 
 const app = express()
+app.use(cors())
+
 app.use(
     "/graphql",
     graphqlHTTP({
